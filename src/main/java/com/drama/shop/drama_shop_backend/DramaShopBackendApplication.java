fb26b599887fa189fbdb5e51b000906dedad79a3
pizarropablox <32684457+pizarropablox@users.noreply.github.com>
2024-12-06 21:29:02 -0300

    first commit

---
+++ src/main/java/com/drama/shop/drama_shop_backend/DramaShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.drama.shop.drama_shop_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DramaShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DramaShopBackendApplication.class, args);
+	}
+
+}
